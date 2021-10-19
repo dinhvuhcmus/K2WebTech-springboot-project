16254902f7e40fe41c6f56858319a01cabc1fa10
Vu Nguyen <dinhvuhcmus@gmail.com>
2021-10-19 18:48:30 +0700

    springboot-project: add unit test

--- springboot-project/src/test/java/co/k2web/springboot/SpringbootProjectApplicationTests.java
+++ springboot-project/src/test/java/co/k2web/springboot/SpringbootProjectApplicationTests.java
@@ -1,14 +1,25 @@
 package co.k2web.springboot;
 
-
-import org.junit.Test;
+import co.k2web.springboot.service.RequestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringbootProjectApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Autowired
+    private RequestService requestService;
+
+    @Test
+    void storeRequest_withAValueRequest_storeSuccessfully() {
+        String value = "REQUEST";
+		Map<String, String> requestMap = requestService.storeRequest(value);
+		assertThat(requestMap.size()).isEqualTo(1);
+	}
 
 }
